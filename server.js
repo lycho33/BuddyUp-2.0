d19ba5e3774ca823681c29c2216fdea71b8cb0e8
lycho33 <cho.lyds@gmail.com>
2022-04-06 22:39:38 -0400

    update CORS and websocket connect with client

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-// const express = require('express');
-// const app = express();
-// const http = require('http');
-// const server = http.createServer(app);
-// const PORT = 8080;
-
-// app.get('/', (req, res) => {
-//   res.send('<h1>Hello world</h1>');
-// });
-
-// server.listen(PORT, () => {
-//   console.log(`listening on *:${PORT}`);
-// });
-
 const express = require('express');
 const app = express();
 const http = require('http');
+const cors = require('cors');
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+app.use(cors())
+
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    }
+  });
+
+server.listen(8080, (socket) => {
+  console.log('listening on *:8080');
 });
 
-server.listen(3001, () => {
-  console.log('listening on *:3001');
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`);
+    socket.on('disconnect', () => {
+      console.log('user disconnected', socket.id);
+    });
 });
