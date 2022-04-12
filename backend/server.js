c38af6962fa55f3e3592f8f11f892d95fc821886
lycho33 <cho.lyds@gmail.com>
2022-04-11 23:31:45 -0400

    added custom event to send message

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,10 @@ io.on('connection', (socket) => {
       console.log(`User with ID: ${socket.id} joined room: ${data}`)
     })
 
+    socket.on("send_message", data => {
+      console.log(data)
+    })
+
     socket.on('disconnect', () => {
       console.log('user disconnected', socket.id);
     });
