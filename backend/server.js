332a07c00b3bbfb28686afbec357a885b2255d53
lycho33 <cho.lyds@gmail.com>
2022-04-11 23:15:09 -0400

    added custom event to join a room

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,15 @@ server.listen(8080, (socket) => {
 
 io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
+
+    socket.on("join_room", (data) => {
+      socket.join(data)
+      console.log(`User with ID: ${socket.id} joined room: ${data}`)
+    })
+
     socket.on('disconnect', () => {
       console.log('user disconnected', socket.id);
     });
 });
+
+
