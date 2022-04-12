0a92268be7aff0cf4df4727391b64103e9e12f7b
lycho33 <cho.lyds@gmail.com>
2022-04-11 23:14:29 -0400

    added a form to join a room

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import io from "socket.io-client";
+import {useState} from 'react'
 
 const SERVER = "http://localhost:8080";
 const socket = io.connect(SERVER)
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
+
+  const joinRoom = () => {
+    if(username !== "" && room !== ""){
+      socket.emit("join_room", room)
+    }
+  }
 
   return (
     <div className="App">
       <h1>Testing</h1>
+
+      <h3>Join a Chat</h3>
+      <input type="text"  placeholder='username' onChange={(e) => setUsername(e.target.value)}/>
+      <input type="text" placeholder='room' onChange={(e) => setRoom(e.target.value)}/>
+      <button onClick={joinRoom}>Join A Room</button>
     </div>
   );
 }
